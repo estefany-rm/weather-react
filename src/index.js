6d5aadec121bb4a40ffc32bc5da063198bff4506
estefany-rm <estefany18.ramirez@gmail.com>
2021-11-03 23:35:42 +0100

    Added footer

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="weather-app-wrapper">
+      <App />
+    </div>
+    <div className="footer">
+      <small>
+        <a
+          href="https://github.com/estefany-rm/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by Estefany Ramirez
+      </small>
+    </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
